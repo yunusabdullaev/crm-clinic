816694a78ba2e5beb8a54abd314cbcd251214876
User <user@example.com>
2026-02-02 00:34:03 +0500

    feat: add security headers to fix CSP warnings

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,35 @@ const nextConfig = {
     env: {
         API_URL: process.env.API_URL || 'http://localhost:8080',
     },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-DNS-Prefetch-Control',
+                        value: 'on'
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block'
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN'
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff'
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'origin-when-cross-origin'
+                    }
+                ]
+            }
+        ];
+    }
 }
 
 module.exports = nextConfig
